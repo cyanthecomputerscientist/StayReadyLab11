f7955ce6f5aaa4c00d21e5ca3b6c45b5e33cde41
Dominique Smith <cyanthecomputerscientist@gmail.com>
2020-07-23 11:43:28 -0700

    changes made

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,24 +10,43 @@ public class App
     //allow user to select a local text file for spell checking
     // display the incorrect spellings and suggested possibilities
     public static HashSet<String> fileToCheck = new HashSet<String>();
-    public static void main(String[] args)
-    {
+    public static HashSet<String> spellCheck = new HashSet<String>();
 
-        String pathname = "C:/Users/Dominique/Documents/Dev/StayReadyLab11/letter_from_gandhi.txt";
+    public static String parse(String input)
+    {
+        input = input.replaceAll("\\p{Punct}", " ");
+        input = input.toLowerCase();
+        return "";
+    }
+
+    public static HashSet<String> readInFile(String pathname)
+    {
+        HashSet<String> hashSet = new HashSet<String>();
         try {
             File file = new File(pathname);
             Scanner scan = new Scanner(file);
-            while (scan.hasNextLine()) {
-                fileToCheck.add(scan.nextLine());
+            while(scan.hasNextLine())
+            {
+                hashSet.add(scan.nextLine());
             }
-            
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             e.printStackTrace();
         }
-        System.out.println(fileToCheck);
+        return hashSet;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+
+        String pathname = "C:/Users/Dominique/Documents/Dev/StayReadyLab11/letter_from_gandhi.txt";
+        fileToCheck = readInFile(pathname);
         //get file from user (check)
         //turn file into hash set (check)
+        String pathname1 = "C:/Users/Dominique/Documents/Dev/StayReadyLab11/words_alpha.txt";
+        spellCheck = readInFile(pathname1);
+
     }
-    
 }
--- src/test/java/SpellCheckerTest.java
+++ src/test/java/SpellCheckerTest.java
@@ -5,6 +5,12 @@ public class SpellCheckerTest {
     @Test
     public void Test()
     {
+        App app = new App();
+        String input = "Hello!!!";
+        String actual = App.parse(input);
+        String expected = "Hello";
+        Ass
+        
 
     }
 }
